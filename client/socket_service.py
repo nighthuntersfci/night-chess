bf470b10f47f0214b60d2bf8172cda266662592e
Mohamed El Dahtowry <salintofficial@hotmail.com>
2023-10-28 09:47:51 +0200

    Add top bar

--- client/socket_service.py
+++ client/socket_service.py
@@ -6,5 +6,5 @@ def set_username(name):
 	sio.emit("set_name", name)
 
 def start():
-	sio.connect("http://localhost:8080", transports=['websocket'])
+	sio.connect("http://localhost:8080")
 	sio.wait()
--- client/views/rooms.py
+++ client/views/rooms.py
@@ -7,3 +7,12 @@ class Rooms(Frame):
 		self.pack(fill = BOTH, expand=True)
 
 		self.parent = parent
+
+		# Top Bar
+		top = Frame(self, background=theme.background_secondary)
+		top.pack(fill=X, side="top")
+		top.grid_columnconfigure(0, weight=1)
+		top.grid_columnconfigure(1, weight=1)
+
+		Label(top, text="Rooms", background=theme.background_secondary, foreground=theme.text_primary, font=("Arial", 20)).grid(row=0, column=0, pady=(10, 0))
+		Button(top, text="Create Room", background=theme.color_primary, foreground=theme.text_primary, border="0", width=20, activebackground=theme.color_secondary, activeforeground=theme.text_primary).grid(row=0, column=1, pady=20)
