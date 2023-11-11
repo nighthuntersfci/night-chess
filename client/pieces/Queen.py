a72e2bdf60622e5ebb66004e9ce73c6cdb77f382
Ahmed-Hunter <ahmedraeda66@gmail.com>
2023-11-11 04:55:02 +0200

    movement  Queen

--- client/pieces/Queen.py
+++ client/pieces/Queen.py
@@ -6,3 +6,33 @@ class Queen(Piece):
             super().__init__(x, y, "assets/w_q.png", color)
         else:
             super().__init__(x, y, "assets/b_q.png", color)
+    def get_moves(self, data):
+        moves = []
+            
+        for d in range(-7,8):
+            if d != 0:  
+                if 0 <= self.x +d  <=7 and 0 <= self.y +d  <= 7:
+                    moves.append([d,d])
+                if 0 <= self.x -d  <=7 and 0 <= self.y -d  <= 7:
+                    moves.append([-d,-d])
+                if 0 <= self.x +d  <=7 and 0 <= self.y -d  <= 7:
+                    moves.append([d,-d])
+                if 0 <= self.x -d  <=7 and 0 <= self.y +d  <= 7:
+                    moves.append([-d,d])
+        for i in range(1, 8):
+            if 0 <= self.x + i <= 7:
+                    moves.append([i, 0]) 
+                
+        for i in range(1, 8):
+            if 0 <= self.x - i <= 7:
+                    moves.append([-i, 0])
+              
+        for i in range(1, 8):
+            if 0 <= self.y + i <= 7:
+                    moves.append([0, i])
+               
+        for i in range(1, 8):
+            if 0 <= self.y - i <= 7:
+                    moves.append([0, -i])
+                
+        return moves
