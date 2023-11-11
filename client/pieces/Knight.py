448ec01edfa005116b1cba6fd2b3179ed83e4424
Ahmed-Hunter <ahmedraeda66@gmail.com>
2023-11-11 03:56:55 +0200

    Added the knight movement

--- client/pieces/Knight.py
+++ client/pieces/Knight.py
@@ -6,3 +6,17 @@ class Knight(Piece):
             super().__init__(x, y, "assets/w_n.png", color)
         else:
             super().__init__(x, y, "assets/b_n.png", color)
+    def is_valid_position(self,x,y):
+        return 0<=x<=7 and 0<=y<=7
+    def get_moves(self, data):
+        moves = []
+            
+        for dx in [1,2,-1,-1]:
+            for dy in [1,2,-1,-2]:
+                if abs(dx) != abs(dy):
+                    new_x = self.x +dx
+                    new_y = self.y +dy
+                    if 0 <= new_x <=7 and 0<=new_y<=7:
+                        moves.append([dx,dy])
+          
+        return moves
