ed98957d38ff20c671ad780131efc8a225c6b080
AhmedHelmy95017 <ahmedmohammedhelmy18112003@gmail.com>
2023-11-13 13:13:13 +0200

    Fix name issue

--- client/views/intro.py
+++ client/views/intro.py
@@ -4,42 +4,86 @@ import theme
 import data
 from views.rooms import Rooms
 
+
 class Intro(Frame):
-	def __init__(self, parent):
-		super().__init__(parent, background=theme.background_primary)
-		self.pack(fill = BOTH, expand=True)
+    def __init__(self, parent):
+        super().__init__(parent, background=theme.background_primary)
+        self.pack(fill=BOTH, expand=True)
 
-		self.parent = parent
+        self.parent = parent
 
-		# Top Bar
-		top = Frame(self, background=theme.background_secondary)
-		top.pack(fill=X, side="top")
-		top.grid_columnconfigure(0, weight=1)
-		top.grid_columnconfigure(2, weight=1)
+        # Top Bar
+        top = Frame(self, background=theme.background_secondary)
+        top.pack(fill=X, side="top")
+        top.grid_columnconfigure(0, weight=1)
+        top.grid_columnconfigure(2, weight=1)
 
-		Label(top, text="Night Chess", background=theme.background_secondary, foreground=theme.text_primary, font=("Arial", 20)).grid(row=0, column=1, pady=(10, 0))
-		Label(top, text="By Night Hunters", background=theme.background_secondary, foreground=theme.text_secondary, font=("Arial", 10)).grid(row=1, column=1, pady=(0, 10))
+        Label(
+            top,
+            text="Night Chess",
+            background=theme.background_secondary,
+            foreground=theme.text_primary,
+            font=("Arial", 20),
+        ).grid(row=0, column=1, pady=(10, 0))
+        Label(
+            top,
+            text="By Night Hunters",
+            background=theme.background_secondary,
+            foreground=theme.text_secondary,
+            font=("Arial", 10),
+        ).grid(row=1, column=1, pady=(0, 10))
 
-		prompt = Frame(self, background=theme.background_primary)
-		prompt.pack(fill=BOTH, expand=True, side="bottom")
-		prompt.grid_columnconfigure(0, weight=1)
-		prompt.grid_columnconfigure(2, weight=1)
-		prompt.grid_rowconfigure(0, weight=1)
-		prompt.grid_rowconfigure(4, weight=1)
+        prompt = Frame(self, background=theme.background_primary)
+        prompt.pack(fill=BOTH, expand=True, side="bottom")
+        prompt.grid_columnconfigure(0, weight=1)
+        prompt.grid_columnconfigure(2, weight=1)
+        prompt.grid_rowconfigure(0, weight=1)
+        prompt.grid_rowconfigure(4, weight=1)
 
-		self.name = StringVar()
+        self.name = StringVar()
 
-		Label(prompt, text="Enter Your Name:", background=theme.background_primary, foreground=theme.text_primary, font=("Arial", 30)).grid(row=1, column=1, pady=(10, 0))
-		entry = Entry(prompt, background="white", textvariable=self.name, foreground="black", font=("Arial", 15), border="0")
-		entry.bind('<Return>' , self.set_username)
-		entry.grid(row=2, column=1)
-		
-		Button(prompt, text="Enter", background=theme.color_primary, foreground=theme.text_primary, border="0", width=20, activebackground=theme.color_secondary, activeforeground=theme.text_primary, command=self.set_username).grid(row=3, column=1, pady=20)
-		
+        Label(
+            prompt,
+            text="Enter Your Name:",
+            background=theme.background_primary,
+            foreground=theme.text_primary,
+            font=("Arial", 30),
+        ).grid(row=1, column=1, pady=(10, 0))
+        entry = Entry(
+            prompt,
+            background="white",
+            textvariable=self.name,
+            foreground="black",
+            font=("Arial", 15),
+            border="0",
+        )
+        entry.bind("<Return>", self.set_username)
+        entry.grid(row=2, column=1)
 
-	def set_username(self,event=0):
-		socket_service.set_username(self.name.get())
-		data.name = self.name.get()
+        Button(
+            prompt,
+            text="Enter",
+            background=theme.color_primary,
+            foreground=theme.text_primary,
+            border="0",
+            width=20,
+            activebackground=theme.color_secondary,
+            activeforeground=theme.text_primary,
+            command=self.set_username,
+        ).grid(row=3, column=1, pady=20)
 
-		data.rooms_window = Rooms(self.parent)
-		self.destroy()
+    def set_username(self, event=0):
+        if len(self.name.get()) <= 8:
+            socket_service.set_username(self.name.get())
+            data.name = self.name.get()
+
+            data.rooms_window = Rooms(self.parent)
+            self.destroy()
+
+        else:
+            error = Label(
+                self,
+                text="Your name must be less or equal than 8 characters.",
+                font=("Arial", 18),
+            )
+            error.pack()
