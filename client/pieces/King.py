4ae01dc847f7ebf2c1169452edc3ad9a27537cf3
mohamed2030644 <mzayd4348@gmail.com>
2023-11-27 13:37:33 +0200

    Fix pawn checking and blocking

--- client/pieces/King.py
+++ client/pieces/King.py
@@ -4,6 +4,7 @@ from pieces.Rook import Rook
 from pieces.Bishop import Bishop
 from pieces.Knight import Knight
 from pieces.Pawn import Pawn
+from pieces.Blank import Blank
 
 class King(Piece):
     def __init__(self, x, y, color):
@@ -26,6 +27,8 @@ class King(Piece):
                             if (isinstance(data[self.x + i[0] + j][self.y + i[1]], Queen) or isinstance(data[self.x + i[0] + j][self.y + i[1]], Rook)) and self.color != data[self.x + i[0] + j][self.y + i[1]].color:  
                                 add = False
                                 break
+                            elif not isinstance(data[self.x + i[0] + j][self.y + i[1]], Blank):
+                                break
                         
                     if add == True:
                         for j in range(0, 7):
@@ -33,14 +36,18 @@ class King(Piece):
                                 if (isinstance(data[(self.x + i[0]) - j][self.y + i[1]], Queen) or isinstance(data[(self.x + i[0]) - j][self.y + i[1]], Rook)) and self.color != data[(self.x + i[0]) - j][self.y + i[1]].color:  
                                     add = False
                                     break
+                                elif not isinstance(data[self.x + i[0] - j][self.y + i[1]], Blank):
+                                    break
                     
                     # Horizontal Movement
                     if add == True:
                         for j in range(0,7):
-                                if 0 <= self.y + i[1] + j <= 7:
-                                    if (isinstance(data[self.x + i[0]][self.y + i[1] + j], Queen) or isinstance(data[self.x + i[0]][(self.y + i[1]) + j], Rook)) and self.color != data[self.x + i[0]][(self.y + i[1]) + j].color:  
-                                        add = False
-                                        break
+                            if 0 <= self.y + i[1] + j <= 7:
+                                if (isinstance(data[self.x + i[0]][self.y + i[1] + j], Queen) or isinstance(data[self.x + i[0]][(self.y + i[1]) + j], Rook)) and self.color != data[self.x + i[0]][(self.y + i[1]) + j].color:  
+                                    add = False
+                                    break
+                                elif not isinstance(data[self.x + i[0]][self.y + i[1] + j], Blank):
+                                    break
                         
                     if add == True:
                         for j in range(0, 7):
@@ -48,29 +55,37 @@ class King(Piece):
                                 if (isinstance(data[self.x + i[0]][(self.y + i[1]) - j], Queen) or isinstance(data[self.x + i[0]][(self.y + i[1]) - j], Rook)) and self.color !=data[self.x + i[0]][(self.y + i[1]) - j].color:  
                                     add = False
                                     break
+                                elif not isinstance(data[self.x + i[0]][self.y + i[1] - j], Blank):
+                                    break
                     
                     # Diagonal Right-side 
                     if add == True:
                         for j in range(0,7):
-                                if 0 <= self.y + i[1] + j <= 7 and 0 <= self.x + i[0] + j <= 7:
-                                    if (isinstance(data[self.x + i[0] + j][self.y + i[1] + j], Queen) or isinstance(data[self.x + i[0] + j][(self.y + i[1]) + j], Bishop)) and self.color !=data[self.x + i[0] + j][(self.y + i[1]) + j].color:  
-                                        add = False
-                                        break
+                            if 0 <= self.y + i[1] + j <= 7 and 0 <= self.x + i[0] + j <= 7:
+                                if (isinstance(data[self.x + i[0] + j][self.y + i[1] + j], Queen) or isinstance(data[self.x + i[0] + j][(self.y + i[1]) + j], Bishop)) and self.color !=data[self.x + i[0] + j][(self.y + i[1]) + j].color:  
+                                    add = False
+                                    break
+                                elif not isinstance(data[self.x + i[0] + j][self.y + i[1] + j], Blank):
+                                    break
                         
                     if add == True:
                         for j in range(0, 7):
                             if 0 <= (self.y + i[1]) - j <= 7 and 0 <= (self.x + i[0]) + j <= 7:
                                 if (isinstance(data[self.x + i[0] + j][(self.y + i[1]) - j], Queen) or isinstance(data[self.x + i[0] + j][(self.y + i[1]) - j], Bishop)) and self.color !=data[self.x + i[0] + j][(self.y + i[1]) - j].color:  
                                     add = False
-                                    break               
+                                    break    
+                                elif not isinstance(data[self.x + i[0] + j][self.y + i[1] - j], Blank):
+                                    break           
 
                     # Diagonal Left Side
                     if add == True:
                         for j in range(0,7):
-                                if 0 <= self.y + i[1] - j <= 7 and 0 <= self.x + i[0] - j <= 7:
-                                    if (isinstance(data[self.x + i[0] - j][self.y + i[1] - j], Queen) or isinstance(data[self.x + i[0] - j][(self.y + i[1]) - j], Bishop)) and self.color !=data[self.x + i[0] - j][(self.y + i[1]) - j].color:  
-                                        add = False
-                                        break
+                            if 0 <= self.y + i[1] - j <= 7 and 0 <= self.x + i[0] - j <= 7:
+                                if (isinstance(data[self.x + i[0] - j][self.y + i[1] - j], Queen) or isinstance(data[self.x + i[0] - j][(self.y + i[1]) - j], Bishop)) and self.color !=data[self.x + i[0] - j][(self.y + i[1]) - j].color:  
+                                    add = False
+                                    break
+                                elif not isinstance(data[self.x + i[0] - j][self.y + i[1] - j], Blank):
+                                    break
                         
                     if add == True:
                         for j in range(0, 7):
@@ -78,6 +93,8 @@ class King(Piece):
                                 if (isinstance(data[self.x + i[0] - j][(self.y + i[1]) + j], Queen) or isinstance(data[self.x + i[0] - j][(self.y + i[1]) + j], Bishop)) and self.color !=data[self.x + i[0] - j][(self.y + i[1]) + j].color:  
                                     add = False
                                     break   
+                                elif not isinstance(data[self.x + i[0] - j][self.y + i[1] + j], Blank):
+                                    break
 
                     # Knight movement 
                     if add == True:
@@ -85,19 +102,20 @@ class King(Piece):
                             break_outer = False
                             for dy in [1, -1, 2, -2]:
                                 if abs(dx) != abs(dy):
-                                    if (isinstance(data[self.x + i[0] + dx][self.y + i[1] + dy], Knight)) and self.color !=data[self.x + i[0] + dx][self.y + i[1] + dy].color: 
-                                        add = False
-                                        break_outer = True
-                                        break
+                                    if 0 <= self.x + i[0] + dx <= 7 and 0 <= self.y + i[1] + dy <= 7:
+                                        if (isinstance(data[self.x + i[0] + dx][self.y + i[1] + dy], Knight)) and self.color !=data[self.x + i[0] + dx][self.y + i[1] + dy].color: 
+                                            add = False
+                                            break_outer = True
+                                            break
                             if break_outer:
                                 break
                     
                     if add == True:
                         if self.color == "W":
-                            if (isinstance(data[self.x + i[0] + 1][self.y + i[1] + 1], Pawn) or isinstance(data[self.x + i[0] + 1][self.y + i[1] - 1], Pawn)) and self.color !=data[self.x + i[0] + 1][self.y + i[1] + 1].color:
+                            if (isinstance(data[self.x + i[0] + 1][self.y + i[1] + 1], Pawn) or isinstance(data[self.x + i[0] + 1][self.y + i[1] - 1], Pawn)) and (data[self.x + i[0] + 1][self.y + i[1] + 1].color == "B" or data[self.x + i[0] + 1][self.y + i[1] - 1].color == "B"):
                                 add = False
                         else:
-                            if (isinstance(data[self.x + i[0] - 1][self.y + i[1] + 1], Pawn) or isinstance(data[self.x + i[0] - 1][self.y + i[1] - 1], Pawn)) and self.color !=data[self.x + i[0] - 1][self.y + i[1] + 1].color:
+                            if (isinstance(data[self.x + i[0] - 1][self.y + i[1] + 1], Pawn) or isinstance(data[self.x + i[0] - 1][self.y + i[1] - 1], Pawn)) and (data[self.x + i[0] - 1][self.y + i[1] + 1].color == "B" or data[self.x + i[0] - 1][self.y + i[1] - 1].color == "B"):
                                 add = False
                     if add: 
                         moves.append(i)
