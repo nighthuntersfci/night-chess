b4d017a569e782b84df30e509c8485c63d2be12b
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2023-12-06 17:10:59 +0200

    Fix is_in_danger function

--- client/pieces/King.py
+++ client/pieces/King.py
@@ -1,6 +1,6 @@
 from pieces.Piece import Piece
-from pieces.Blank import Blank
 from utils.pieces import is_in_danger
+
 
 class King(Piece):
     def __init__(self, x, y, color):
@@ -8,14 +8,22 @@ class King(Piece):
             super().__init__(x, y, "assets/w_k.png", color)
         else:
             super().__init__(x, y, "assets/b_k.png", color)
+
     def get_moves(self, data):
         moves = []
-        possible_moves = [(1,1),(-1,-1),(-1,1),(1,-1),(0,1),(0,-1),(1,0),(-1,0)]
+        possible_moves = [
+            (1, 1),
+            (-1, -1),
+            (-1, 1),
+            (1, -1),
+            (0, 1),
+            (0, -1),
+            (1, 0),
+            (-1, 0),
+        ]
         for i in possible_moves:
             if 0 <= self.x + i[0] <= 7 and 0 <= self.y + i[1] <= 7:
                 if self.color != data[self.x + i[0]][self.y + i[1]].color:
-                    data_snapshot = data
-                    data_snapshot[self.x][self.y] = Blank(self.x, self.y)
-                    if not is_in_danger(self.x + i[0], self.y + i[1], self.color, data_snapshot): 
+                    if not is_in_danger(self.x + i[0], self.y + i[1], self.color, data):
                         moves.append(i)
         return moves
